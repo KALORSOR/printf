2fcdb1dfa3fe47723f3fa027b4e9408553e1a069
KALORSOR <alorsorgodsway@gmail.com>
2023-11-15 07:37:06 -0800

    _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
+* _putchar - writes character c to standard output
+* @c: character to print
 * Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* On error, -1 is returned, and error is set.
 */
 int _putchar(char c)
 {
