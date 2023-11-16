9a35e09793be28b78f53836710bdbc9e12b554e5
DiyaGariba <hamdiya19tobe@gmail.com>
2023-11-15 18:18:23 -0800

    update

--- print_pointer.c
+++ print_pointer.c
@@ -8,7 +8,7 @@ int print_pointer(va_list args)
 {
 void *p;
 char *s = "(nil)";
-long int x;
+long int a;
 int y;
 int i;
 p = va_arg(args, void*);
@@ -20,9 +20,9 @@ _putchar(s[i]);
 }
 return (i);
 }
-x = (unsigned long int)p;
+a = (unsigned long int)p;
 _putchar('0');
 _putchar('x');
-y = print_hex_extra(x);
+y = print_hex_extra(a);
 return (y + 2);
 }
